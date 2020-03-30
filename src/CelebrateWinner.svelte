051cdbdd2575f7d7d93a1ac7fa464c1d05db358b
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2020-03-30 14:55:54 +0200

    Handle disabled autoplay on audio element

--- src/CelebrateWinner.svelte
+++ src/CelebrateWinner.svelte
@@ -9,6 +9,15 @@
   onMount(() => {
     new CanvasAnimation(document.getElementById("confetti"), 0.66);
     document.documentElement.style.setProperty("--bg-color", winner.color);
+
+    document.getElementById("cheer").addEventListener("canplaythrough", ev => {
+      if (ev.target.paused) {
+        // If browser has disabled autoplay
+        muteAudio.call(ev.target.nextElementSibling);
+      } else {
+        ev.target.nextElementSibling.hidden = false;
+      }
+    });
   });
 
   function muteAudio() {
@@ -48,7 +57,7 @@
     }
   }
 
-  svg {
+  button {
     position: absolute;
     top: 1rem;
     right: 1rem;
@@ -56,6 +65,14 @@
     height: 1rem;
 
     cursor: pointer;
+
+    -webkit-appearance: none;
+    appearance: none;
+    color: inherit;
+    padding: 0;
+    margin: 0;
+    border: none;
+    background: none;
   }
 
   footer {
@@ -73,24 +90,25 @@
     <source src="cheer.ogg" type="audio/ogg" />
     Your browser does not support this audio format.
   </audio>
-  <svg
-    aria-hidden="true"
-    data-icon="volume-mute"
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 512 512"
-    on:click={muteAudio}>
-    <path
-      fill="currentColor"
-      d="M215.03 71.05L126.06 160H24c-13.26 0-24 10.74-24 24v144c0 13.25 10.74
-      24 24 24h102.06l88.97 88.95c15.03 15.03 40.97 4.47
-      40.97-16.97V88.02c0-21.46-25.96-31.98-40.97-16.97zM461.64
-      256l45.64-45.64c6.3-6.3 6.3-16.52
-      0-22.82l-22.82-22.82c-6.3-6.3-16.52-6.3-22.82 0L416
-      210.36l-45.64-45.64c-6.3-6.3-16.52-6.3-22.82 0l-22.82 22.82c-6.3 6.3-6.3
-      16.52 0 22.82L370.36 256l-45.63 45.63c-6.3 6.3-6.3 16.52 0 22.82l22.82
-      22.82c6.3 6.3 16.52 6.3 22.82 0L416 301.64l45.64 45.64c6.3 6.3 16.52 6.3
-      22.82 0l22.82-22.82c6.3-6.3 6.3-16.52 0-22.82L461.64 256z" />
-  </svg>
+  <button on:click={muteAudio} hidden>
+    <svg
+      aria-hidden="true"
+      data-icon="volume-mute"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 512 512">
+      <path
+        fill="currentColor"
+        d="M215.03 71.05L126.06 160H24c-13.26 0-24 10.74-24 24v144c0 13.25 10.74
+        24 24 24h102.06l88.97 88.95c15.03 15.03 40.97 4.47
+        40.97-16.97V88.02c0-21.46-25.96-31.98-40.97-16.97zM461.64
+        256l45.64-45.64c6.3-6.3 6.3-16.52
+        0-22.82l-22.82-22.82c-6.3-6.3-16.52-6.3-22.82 0L416
+        210.36l-45.64-45.64c-6.3-6.3-16.52-6.3-22.82 0l-22.82 22.82c-6.3 6.3-6.3
+        16.52 0 22.82L370.36 256l-45.63 45.63c-6.3 6.3-6.3 16.52 0 22.82l22.82
+        22.82c6.3 6.3 16.52 6.3 22.82 0L416 301.64l45.64 45.64c6.3 6.3 16.52 6.3
+        22.82 0l22.82-22.82c6.3-6.3 6.3-16.52 0-22.82L461.64 256z" />
+    </svg>
+  </button>
 </main>
 
 <canvas id="confetti" />
